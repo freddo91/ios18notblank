fd888188583fb951037f4faf5c89af7375a47e79
smiles.plz91@gmail.com <ios18@mac06.local>
2013-09-30 21:33:36 +0700

    add class hoanglinh

---
+++ demogit04/demogit04/hoanglinh.h
@@ -0,0 +1,13 @@
+//
+//  hoanglinh.h
+//  demogit04
+//
+//  Created by iOS18 on 9/30/13.
+//  Copyright (c) 2013 iOS18. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface hoanglinh : NSObject
+
+@end
---
+++ demogit04/demogit04/hoanglinh.m
@@ -0,0 +1,13 @@
+//
+//  hoanglinh.m
+//  demogit04
+//
+//  Created by iOS18 on 9/30/13.
+//  Copyright (c) 2013 iOS18. All rights reserved.
+//
+
+#import "hoanglinh.h"
+
+@implementation hoanglinh
+
+@end
